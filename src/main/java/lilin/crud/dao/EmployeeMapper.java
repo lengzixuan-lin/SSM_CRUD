937e924fb74567ae2ac0dda233872e4b74235aef
tom_glb <goodMorning_glb@atguigu.com>
2020-06-02 21:04:13 +0800

    "SSM环境的搭建，以及mybitis逆向工程"

---
+++ src/main/java/lilin/crud/dao/EmployeeMapper.java
@@ -0,0 +1,34 @@
+package lilin.crud.dao;
+
+import java.util.List;
+import lilin.crud.bean.Employee;
+import lilin.crud.bean.EmployeeExample;
+import org.apache.ibatis.annotations.Param;
+
+public interface EmployeeMapper {
+    long countByExample(EmployeeExample example);
+
+    int deleteByExample(EmployeeExample example);
+
+    int deleteByPrimaryKey(Integer empId);
+
+    int insert(Employee record);
+
+    int insertSelective(Employee record);
+
+    List<Employee> selectByExample(EmployeeExample example);
+
+    Employee selectByPrimaryKey(Integer empId);
+
+    List<Employee> selectByExampleWithDept(EmployeeExample example);
+
+    Employee selectByPrimaryKeyWithDept(Integer empId);
+
+    int updateByExampleSelective(@Param("record") Employee record, @Param("example") EmployeeExample example);
+
+    int updateByExample(@Param("record") Employee record, @Param("example") EmployeeExample example);
+
+    int updateByPrimaryKeySelective(Employee record);
+
+    int updateByPrimaryKey(Employee record);
+}
---
+++ target/My_SSM_CRUD/index.jsp
@@ -0,0 +1,20 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: lin
+  Date: 2020/6/1
+  Time: 20:52
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+    <script type="text/javascript" src="static/js/jquery-3.1.1.js"></script>
+    <link href="static/bootstrap-3.3.7-dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="static/bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <button class="btn btn-success">按钮</button>
+
+</body>
+</html>
